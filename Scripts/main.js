f35559c21fdafd8db97377d466b437feff0ff12b
よね/Yone <66835584+yone1130@users.noreply.github.com>
2022-10-19 15:42:48 +0900

    Update

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,74 +5,104 @@
 // Licensed under the MIT License
 // 
 
-let box_loc_H             = -1;
-let box_locLimit_lower_H  = 0;
-let box_locLimit_higher_H = 100;
-let box_loc_Y             = -1;
-let box_locLimit_lower_Y  = 0;
-let box_locLimit_higher_Y = 100;
-
-let moveVal               = 1/4;
-let mode_H                = -1;
-let mode_Y                = -1;
-let modeChgCnt            = -1;
-let modeChgInterval       = -1;
-
-const box = document.getElementById("box");
-
-// ---------- Main ---------- //
-mainloop();
+let boxes = [];
 
 // ---------- Functions ---------- //
 function mainloop(){
 
-    // Random
-    modeChgCnt++;
-
-    if (modeChgCnt >= modeChgInterval || modeChgCnt == -1){
-        mode_H = rnd(2)
-        mode_Y = rnd(2)
-        modeChgInterval = rnd(120);
-        modeChgCnt = 0;
+    for (i = 0; i <= boxes.length-1; i++){
+        boxes[i].move(i);
     }
-
-    // Mover
-    if (mode_H == 0){
-        box_loc_H += moveVal;
-        if (box_loc_H > box_locLimit_higher_H){
-            mode_H = 1;
-            box_loc_H = box_locLimit_higher_H
-        }
-    } else if (mode_H == 1){
-        box_loc_H -= moveVal;
-        if (box_loc_H < box_locLimit_lower_H){
-            mode_H = 0;
-            box_loc_H = box_locLimit_lower_H
-        }
-    }
-
-    if (mode_Y == 0){
-        box_loc_Y += moveVal;
-        if (box_loc_Y > box_locLimit_higher_Y){
-            mode_Y = 1;
-            box_loc_Y = box_locLimit_higher_Y
-        }
-    } else if (mode_Y == 1){
-        box_loc_Y -= moveVal;
-        if (box_loc_Y < box_locLimit_lower_Y){
-            mode_Y = 0;
-            box_loc_Y = box_locLimit_lower_Y
-        }
-    }
-
-    // Set
-    box.style.top = `${box_loc_H}vh`;
-    box.style.left = `${box_loc_Y}vw`;
 
     requestAnimationFrame(mainloop);
+}
+
+// ----- Box ----- //
+class Box{
+    constructor(){
+        this.box_loc_H             = rnd(100);
+        this.box_locLimit_lower_H  = 0;
+        this.box_locLimit_higher_H = 100;
+        this.box_loc_Y             = rnd(100);
+        this.box_locLimit_lower_Y  = 0;
+        this.box_locLimit_higher_Y = 100;
+
+        this.moveVal               = 1/4;
+        this.mode_H                = -1;
+        this.mode_Y                = -1;
+        this.modeChgCnt            = -1;
+        this.modeChgInterval       = -1;
+
+        document.getElementById("main").innerHTML += `
+            <div class="box" id=${boxes.length}></div>
+        `;
+        this.box = document.getElementById(boxes.length);
+
+        this.box.style.top = `${this.box_loc_H}vh`;
+        this.box.style.left = `${this.box_loc_Y}vw`;
+        this.box.style.background = `#${rnd(12700)}`;
+
+        this.box.addEventListener('click', function(){
+            boxes[boxes.length] = new Box();
+        })
+    }
+
+    move(id){
+        this.box = document.getElementById(id);
+
+        // Random
+        this.modeChgCnt++;
+
+        if (this.modeChgCnt >= this.modeChgInterval || this.modeChgCnt == -1){
+            this.mode_H = rnd(2)
+            this.mode_Y = rnd(2)
+            this.modeChgInterval = rnd(120);
+            this.modeChgCnt = 0;
+        }
+
+        // Mover
+        if (this.mode_H == 0){
+            this.box_loc_H += this.moveVal;
+            if (this.box_loc_H > this.box_locLimit_higher_H){
+                this.mode_H = 1;
+                this.box_loc_H = this.box_locLimit_higher_H
+            }
+        } else if (this.mode_H == 1){
+            this.box_loc_H -= this.moveVal;
+            if (this.box_loc_H < this.box_locLimit_lower_H){
+                this.mode_H = 0;
+                this.box_loc_H = this.box_locLimit_lower_H
+            }
+        }
+
+        if (this.mode_Y == 0){
+            this.box_loc_Y += this.moveVal;
+            if (this.box_loc_Y > this.box_locLimit_higher_Y){
+                this.mode_Y = 1;
+                this.box_loc_Y = this.box_locLimit_higher_Y
+            }
+        } else if (this.mode_Y == 1){
+            this.box_loc_Y -= this.moveVal;
+            if (this.box_loc_Y < this.box_locLimit_lower_Y){
+                this.mode_Y = 0;
+                this.box_loc_Y = this.box_locLimit_lower_Y
+            }
+        }
+
+        // Set
+        this.box.style.top = `${this.box_loc_H}vh`;
+        this.box.style.left = `${this.box_loc_Y}vw`;
+    }
 }
 
 // ----- Random ----- //
 function rnd(max) {
     return Math.floor(Math.random() * max);
 }
+
+// ---------- Main ---------- //
+for (i = 0; i < 20; i++){
+    boxes[i] = new Box();
+}
+
+mainloop();
